fb206fa98f2623ca80af0337935a40cba575a308
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-10-31 12:13:01 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GurevskayaVE.Sprint3.TaskReview.V27.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tyuiu.GurevskayaVE.Sprint3.TaskReview.V27.Lib
+{
+    public class DataService
+    {
+        public double[] Calc(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int cnt = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                
+                y = Math.Round((Math.Cos(x) / (x + 1) - Math.Cos(x) * 1.3 + 3 * x), 2);
+                valueArray[4] = 0;
+                valueArray[cnt] = y;
+                cnt++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.GurevskayaVE.Sprint3.TaskReview.V27/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GurevskayaVE.Sprint3.TaskReview.V27.Lib;
+
+namespace Tyuiu.GurevskayaVE.Sprint3.TaskReview.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнила: Гурьевская В.Е. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: SprintReview                                                      *");
+            Console.WriteLine("* Задание #Review                                                         *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнила: Гурьевская В.Е. | ПКТб-23-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("* f(x) = cos(x)/(x+1) - cos(x)*1.3 +3x                                    *");
+            Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне [-5;5]            *");
+            Console.WriteLine("* с шагом 1. Произвести проверку деления на ноль. При делении на ноль     *");
+            Console.WriteLine("* вернуть значение ноль. Округлить до 2 знаков после запятой.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            int startValue = -5;
+            int stopValue = 5;
+
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.Calc(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.Calc(startValue, stopValue);
+
+
+
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|   x      |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |   {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+
+            Console.WriteLine("+----------+-----------+");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
